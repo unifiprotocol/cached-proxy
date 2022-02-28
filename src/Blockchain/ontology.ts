07fefdc9938e32e76ee84e47a51fcc790a9b8dd4
Daniel Blanco Parla <dblancoparla@gmail.com>
2022-02-28 09:13:53 +0100

    feat: add ONT nodes

--- src/Blockchain/ontology.ts
+++ src/Blockchain/ontology.ts
@@ -1,3 +1,13 @@
-export const MAINNET_NODES: Array<string> = [];
+export const MAINNET_NODES: Array<string> = [
+  'https://dappnode1.ont.io:10339',
+  'https://dappnode2.ont.io:10339',
+  'https://dappnode3.ont.io:10339',
+  'https://dappnode4.ont.io:10339'
+];
 
-export const TESTNET_NODES: Array<string> = ['https://polaris1.ont.io:10339/'];
+export const TESTNET_NODES: Array<string> = [
+  'https://polaris1.ont.io:10339/',
+  'https://polaris2.ont.io:10339',
+  'https://polaris3.ont.io:10339',
+  'https://polaris4.ont.io:10339'
+];
