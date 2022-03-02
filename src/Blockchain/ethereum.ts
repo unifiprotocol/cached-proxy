81c7c5f4c8d80a4d69b85107f2c65d43251ee5fd
Daniel Blanco Parla <dblancoparla@gmail.com>
2022-03-02 10:41:11 +0100

    fix: ethereum infura nodes

--- src/Blockchain/ethereum.ts
+++ src/Blockchain/ethereum.ts
@@ -3,12 +3,12 @@
 export const MAINNET_NODES: Array<string> = [
   'https://cloudflare-eth.com',
   'https://main-light.eth.linkpool.io',
-  'https://mainnet.infura.io/v3/7a03b9830d0241c7a5a21183912a660a'
+  'https://mainnet.infura.io/v3/0ff955c9016745feb32f758355fb0976'
 ];
 
 export const TESTNET_NODES: Array<string> = [
-  'https://ropsten.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161',
-  'https://rinkey.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161',
-  'https://goerli.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161',
-  'https://kovan.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161'
+  'https://ropsten.infura.io/v3/0ff955c9016745feb32f758355fb0976',
+  'https://rinkey.infura.io/v3/0ff955c9016745feb32f758355fb0976',
+  'https://goerli.infura.io/v3/0ff955c9016745feb32f758355fb0976',
+  'https://kovan.infura.io/v3/0ff955c9016745feb32f758355fb0976'
 ];
