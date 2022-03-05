a49a8a6361a761c3fd3ae9ccbdf771d644ec404d
Daniel Blanco Parla <dblancoparla@gmail.com>
2022-03-05 19:03:39 +0100

    feat: add internal FTM node

--- src/Blockchain/fantom.ts
+++ src/Blockchain/fantom.ts
@@ -1,6 +1,9 @@
 // https://rpc.info/
 
-export const MAINNET_NODES: Array<string> = ['https://rpc.ftm.tools/'];
+export const MAINNET_NODES: Array<string> = [
+  'https://rpc.ftm.tools/',
+  'http://128.199.33.233:18545'
+];
 
 export const TESTNET_NODES: Array<string> = [
   'https://rpc.testnet.fantom.network/'
