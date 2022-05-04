cafe0f2384ffb6b69e9c90f7d04cb7182c7aeafe
Daniel Blanco Parla <dblancoparla@gmail.com>
2022-05-04 18:44:27 +0200

    feat: nodes

--- src/Blockchain/avalanche.ts
+++ src/Blockchain/avalanche.ts
@@ -2,7 +2,8 @@
 
 export const MAINNET_NODES: Array<string> = [
   // 'https://speedy-nodes-nyc.moralis.io/447e4a84da6e53dc149f22a6/avalanche/mainnet',
-  // 'https://www.ankr.com/protocol/public/avalanche',
+  'https://www.ankr.com/protocol/public/avalanche',
+  'https://api.avax.network/ext/bc/C/rpc',
   'http://3.17.71.172:9650/ext/bc/C/rpc'
 ];
 
--- src/Blockchain/harmony.ts
+++ src/Blockchain/harmony.ts
@@ -1,8 +1,8 @@
 // https://docs.harmony.one/home/developers/api#development-environments
 
 export const MAINNET_NODES: Array<string> = [
-  // 'https://api.harmony.one/',
-  // 'https://api.s0.t.hmny.io/',
+  'https://api.harmony.one/',
+  'https://api.s0.t.hmny.io/',
   'http://13.58.113.115:9500/'
 ];
 
