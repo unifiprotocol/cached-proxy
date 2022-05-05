e1d6e583a7d3cf1250bc6dcf3e8749e3188b6b27
Daniel Blanco Parla <dblancoparla@gmail.com>
2022-05-05 07:14:45 +0200

    feat: remove ankr nodes

--- src/Blockchain/fantom.ts
+++ src/Blockchain/fantom.ts
@@ -6,8 +6,7 @@ export const MAINNET_NODES: Array<string> = [
   'https://rpc.fantom.network/',
   'https://rpc2.fantom.network/',
   'https://rpc3.fantom.network/',
-  'https://rpcapi.fantom.network/',
-  'https://rpc.ankr.com/fantom'
+  'https://rpcapi.fantom.network/'
 ];
 
 export const TESTNET_NODES: Array<string> = [
