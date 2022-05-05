fe9edeaa5471676f9acf0a7883ce1dc45422c9f7
Daniel Blanco Parla <dblancoparla@gmail.com>
2022-05-05 07:06:26 +0200

    chore: fantom

--- src/Blockchain/fantom.ts
+++ src/Blockchain/fantom.ts
@@ -1,8 +1,13 @@
 // https://rpc.info/
 
 export const MAINNET_NODES: Array<string> = [
-  'http://128.199.33.233:18545',
-  'https://rpc.ftm.tools/'
+  // 'http://128.199.33.233:18545',
+  'https://rpc.ftm.tools/',
+  'https://rpc.fantom.network/',
+  'https://rpc2.fantom.network/',
+  'https://rpc3.fantom.network/',
+  'https://rpcapi.fantom.network/',
+  'https://rpc.ankr.com/fantom'
 ];
 
 export const TESTNET_NODES: Array<string> = [
