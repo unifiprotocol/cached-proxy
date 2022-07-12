a6535a66d551763268343eea94a7d835e643b118
Sergio <sergiazow@gmail.com>
2022-07-12 11:41:03 +0200

    fix: set correct target

--- src/balancer.ts
+++ src/balancer.ts
@@ -13,7 +13,7 @@ app.use(cors());
 app.use(
   '/',
   createProxyMiddleware({
-    target: 'https://proxy.unifiprotocol.com',
+    target: 'https://proxy.unifiprotocol.com/worker',
     router: getRandomWorker,
     changeOrigin: true
   })
