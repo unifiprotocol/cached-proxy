84a75d93145251b51bbf038a716aed8be2828214
Daniel Blanco Parla <dblancoparla@gmail.com>
2022-08-24 11:52:22 +0200

    fix: fantom nodes

--- src/Blockchain/fantom.ts
+++ src/Blockchain/fantom.ts
@@ -6,7 +6,8 @@ export const MAINNET_NODES: Array<string> = [
   'https://rpc.fantom.network/',
   'https://rpc2.fantom.network/',
   'https://rpc3.fantom.network/',
-  'https://rpcapi.fantom.network/'
+  'https://rpcapi.fantom.network/',
+  'https://rpc.ankr.com/fantom'
 ];
 
 export const TESTNET_NODES: Array<string> = [
