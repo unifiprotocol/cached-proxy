18189e30cce1b599ec359eabb9add553360c1fb8
Daniel Blanco Parla <dblancoparla@gmail.com>
2022-08-25 19:31:15 +0200

    fix(FTM): testing node

--- src/Blockchain/fantom.ts
+++ src/Blockchain/fantom.ts
@@ -7,7 +7,8 @@ export const MAINNET_NODES: Array<string> = [
   'https://rpc2.fantom.network/',
   'https://rpc3.fantom.network/',
   'https://rpcapi.fantom.network/',
-  'https://rpc.ankr.com/fantom'
+  'https://rpc.ankr.com/fantom',
+  'https://wandering-dry-frog.fantom.discover.quiknode.pro/edb5c3df86b006bc1de8409806dd828ea33e3d3b/' // Testing
 ];
 
 export const TESTNET_NODES: Array<string> = [
